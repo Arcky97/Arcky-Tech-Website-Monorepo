68f030e1a74b01cf10ebff78947b9d3942211d29
Arcky97 <syntixis@gmail.com>
2026-04-25 19:01:53 +0200

    env fix and common error pages

--- packages/ui/src/components/errors/NotFound.tsx
+++ packages/ui/src/components/errors/NotFound.tsx
@@ -1,4 +1,4 @@
-export function NotFound() {
+export function NotFoundComp() {
   return (
     <div className="flex flex-col h-full items-center justify-center text-white">
       <h1 className="text-3xl font-bold">404 | This page was not found.</h1>
