cb97a248a0acf8d8697c66e2f6a08a40eb504109
Arcky97 <syntixis@gmail.com>
2026-05-02 22:59:34 +0200

    updates log added

--- apps/web/components/ProjectUpdateCards.jsx
+++ apps/web/components/ProjectUpdateCards.jsx
@@ -3,6 +3,7 @@ import { getProjectUpdateData } from "@/lib/getProjectUpdateData";
 export async function ProjectUpdateCards() {
   const updates = await getProjectUpdateData(5);
 
+  console.log(updates);
   return(
     <>
       {updates}
