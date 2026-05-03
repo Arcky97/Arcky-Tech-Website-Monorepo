0b79dad13fda75f2ca7e6da41f83190b20e3816a
Arcky97 <syntixis@gmail.com>
2026-05-03 21:37:33 +0200

    alignment and date fixed

--- apps/web/components/ProjectUpdateCards.tsx
+++ apps/web/components/ProjectUpdateCards.tsx
@@ -6,6 +6,10 @@ export async function ProjectUpdateCards() {
   const updates = await getProjectUpdateData();
   const mdxComponents = useMDXComponents({});
 
+  const convertDate = (date: string) => {
+    return new Date(date).toLocaleDateString()
+  };
+
   return (
     <section className="w-7/8 lg:w-6/8">
       <h2 className="head2 sticky top-0 w-full bg-gray-900 px-2 pb-2 z-75">Recent Project Updates</h2>
@@ -13,16 +17,16 @@ export async function ProjectUpdateCards() {
         {updates.map((data, index) => (
           <div
             key={`project-${index}`}
-            className="w-full md:w-[45%] 2xl:w-[31%] bg-gray-800 p-4 rounded-lg shadow"
+            className="w-full md:w-[45%] 2xl:w-[31%] bg-gray-800 p-4 rounded-lg shadow text-center"
           >
-            <h3 className="text-2xl font-semibold">{data.title}</h3>
-            <p className="text-md text-gray-400 italic pt-2">
-              {data.date} • {data.project}
+            <h3 className="text-2xl font-semibold text-center">{data.title}</h3>
+            <p className="text-md text-gray-400 italic pt-2 text-center">
+              {convertDate(data.date)} • {data.project}
             </p>
             <div className="prose prose-invert px-4">
               <MDXRemote source={data.excerpt} components={mdxComponents} />
             </div>
-            <a href={data.slug} className="inline-block mt-1 text-blue-500 hover:font-bold">Read more</a>
+            <a href={data.slug} className="inline-block mt-1 text-blue-500 hover:font-bold text-center">Read more</a>
           </div>
         ))}
       </div>
