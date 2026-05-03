2e88f2e62aa13a09f557cc22d1e377431d309d35
Arcky97 <syntixis@gmail.com>
2026-05-03 21:15:00 +0200

    updates is now server rendered

--- apps/web/components/ProjectUpdateCards.tsx
+++ apps/web/components/ProjectUpdateCards.tsx
@@ -1,33 +1,16 @@
-"use client";
-import { getProjectUpdateData, ProjectUpdate } from "@/lib/getProjectUpdateData";
+import { getProjectUpdateData } from "@/lib/getProjectUpdateData";
 import { useMDXComponents } from "@/mdx-components";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import { useEffect, useState } from "react";
 
-export function ProjectUpdateCards() {
-  const [updateData, setUpdateData] = useState<ProjectUpdate[]>([]);
+export async function ProjectUpdateCards() {
+  const updates = await getProjectUpdateData();
   const mdxComponents = useMDXComponents({});
 
-useEffect(() => {
-  if (process.env.NODE_ENV !== "production") return;
-
-  const fetchData = () => {
-    try {
-      //const data = getProjectUpdateData(5);
-      //setUpdateData(data);
-    } catch (error) {
-      console.error("Failed to fetch updates:", error);
-    }
-  };
-
-  fetchData();
-}, []);
-  
   return (
     <section className="w-7/8 lg:w-6/8">
       <h2 className="head2 sticky top-0 w-full bg-gray-900 px-2 pb-2 z-75">Recent Project Updates</h2>
       <div className="flex flex-wrap justify-center gap-8 p-4">
-        {updateData.map((data, index) => (
+        {updates.map((data, index) => (
           <div
             key={`project-${index}`}
             className="w-full md:w-[45%] 2xl:w-[31%] bg-gray-800 p-4 rounded-lg shadow"
