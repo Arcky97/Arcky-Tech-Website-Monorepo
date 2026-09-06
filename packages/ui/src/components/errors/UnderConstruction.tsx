7c51186b1cd46fd9713474ea39e3725ad80e850a
Arcky97 <syntixis@gmail.com>
2026-09-06 21:47:50 +0200

    very small change for docs

--- packages/ui/src/components/errors/UnderConstruction.tsx
+++ packages/ui/src/components/errors/UnderConstruction.tsx
@@ -4,7 +4,7 @@ export function UnderConstruction() {
   return (
     <ErrorMain
       sub="Under Construction"
-      title="This Page is under construction"
+      title="This Page is under construction!"
       description="We are working hard to get this page finished as soon as possible. Please come back later!"
     />
   );
